27fb4614803088dd50964c146f7909f13c68bd2c
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-10-04 23:27:34 -0400

    commit tree d13b9bd985504ba2fac92cc72ed48357497055cb

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+
+function submitData(username, userEmail){
+    const userData = {
+        name: username,
+        email: userEmail,
+    };
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify(userData),
+    };
+
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(resp => resp.json())
+    .then(function(jsonObj){
+        const body = document.querySelector('body');
+        const p = document.createElement('p');
+
+        p.innerHTML = jsonObj.id;
+
+        body.appendChild(p);
+    
+    })
+    .catch(function(error){
+        const body = document.querySelector('body');
+        const p = document.createElement('p');
+
+        p.innerHTML = error.message;
+
+        body.appendChild(p);
+    })
+
+
+}
